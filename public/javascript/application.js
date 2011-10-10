0f6d1f4a39490428a07141ff0d2802ee0c4c3cf1
Luke Williams <luke@red-root.com>
2011-10-10 13:00:05 +0100

    uSwitch Mobile Version

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-  var timer = $.timer(300000, function (timer) {
+  var timer = $.timer(12000, function (timer) {
     getUpdate();
   });
 
   $('body').dblclick(function(){
     getUpdate();
   });
+  
+  var working = false;
+  
+  function getUpdate(){
+    if(working == false){
+
+      var before = $("#message").html();
+      working = true;
+      $.ajax({
+        url: "/update",
+        cache: true,
+        timeout: 10000,
+        success: function(html){
+          $("#message").html(html).removeClass('error');
+        },
+        error: function(html){
+          $("#message").html('ERROR!!!!').addClass('error');
+        },
+        complete: function(){
+         working = false;
+         timer.reset;
+        }
+      });
+    }
+  }
 
 });
 
-function playAlert(){
-  // this only works in safari
-  var audio = new Audio("../audio/beep.wav");
-  audio.play();
-}
 
-function getUpdate(){
-  var before = $("#message").html();
-  $.ajax({
-    url: "/update",
-    cache: true,
-    timeout: 10000,
-    success: function(html){
-      $("#message").html(html).removeClass('error');
-    },
-    error: function(html){
-      $("#message").html('ERROR!!!!').addClass('error');
-    },
-    complete: function(){
-      if (before != $("#message").html()){
-        playAlert();
-      }
-    }
-  });
-  timer.reset;
-}
